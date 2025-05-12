485ad050ef860a59b8d5e00b768601603331754e
Jaymin Parmar <jaimins100@gmail.com>
2025-05-12 16:20:44 +0530

    Fetch db url from SSM store
    -  assign role to lambda

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const AWS = require("aws-sdk");
 const { neon, neonConfig } = require("@neondatabase/serverless")
-const app = express();
+
+const AWS_REGION = "us-east-1";
+const STAGE = process.env.STAGE || "prod";
+
+const ssm = new AWS.SSM({
+  region: AWS_REGION,
+});
+
+const DATABASE_URL_SSM_PARAM = `/serverless-nodejs-api/${STAGE}/database-url`
 
 async function dbClient() {
   // for http connections
   // non-pooling
+
+  const paramStoreData = await ssm.getParameter({
+    Name: DATABASE_URL_SSM_PARAM,
+    WithDecryption: true,
+  }).promise();
+
   neonConfig.fetchConnectionCache = true
-  const sql = neon(process.env.DATABASE_URL);
+  const sql = neon(paramStoreData.Parameter.Value);
   return sql;
 }
+
+const app = express();
 
 app.get("/", async (req, res, next) => {
   const db = await dbClient();
 
-  const [results] = await db`select now();`
-  
+  const [dbNow] = await db`select now();`
+  const delta = (Date.now() - dbNow.now.getTime()) / 1000;
+
   return res.status(200).json({
     message: "Hello from root!",
-    results: results.now,
+    delta,
   });
 });
 
